56adb7c8388ee2c20e1b2f99be6248ea37bff7e3
Mateusz Łoskot <mateusz@loskot.net>
2022-09-22 13:31:51 +0100

    Fix typo in comment: includeFileNames configuration property name (#79)

--- LogMonitor/src/LogMonitor/LogFileMonitor.cpp
+++ LogMonitor/src/LogMonitor/LogFileMonitor.cpp
@@ -1714,7 +1714,7 @@ LogFileMonitor::ReadLogFile(
                 }
 
                 //
-                //log the name of the source log file if includeFileName field if true
+                //log the name of the source log file if includeFileNames field if true
                 //
                 if (m_includeFileNames)
                 {
