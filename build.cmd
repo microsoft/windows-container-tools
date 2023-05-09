64978fe6921de34ce1bec6836e3ba0e96df346c0
Bob Sira <sbobfitz2@gmail.com>
2023-05-09 02:47:19 -0700

    update to release build script (#139)

--- build.cmd
+++ build.cmd
@@ -1,1 +1,15 @@
-msbuild LogMonitor\LogMonitor.sln /p:platform=x64 /p:configuration=Release
+@cls
+@echo.*************************************************************
+@echo.This script simply calls msbuild to build LogMonitor.exe
+@echo.*************************************************************
+@echo.
+msbuild LogMonitor\LogMonitor.sln /m /p:platform=x64 /p:configuration=Release
+@if '%ERRORLEVEL%' == '0' (
+    echo.
+    echo.
+    echo.*************************************************************
+    echo.               The build was successful!
+    echo.The output should be in LogMonitor\x64\Release\LogMonitor.exe
+    echo.This is the only file needed to deploy the program.
+    echo.*************************************************************
+)
