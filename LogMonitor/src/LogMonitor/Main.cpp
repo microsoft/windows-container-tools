30d5b9dd2c976895d144bc812817d8fc9a049f78
Anthony Nandaa <profnandaa@gmail.com>
2023-05-24 15:19:12 +0300

    fix: move versioning to SemVer format

--- LogMonitor/src/LogMonitor/Main.cpp
+++ LogMonitor/src/LogMonitor/Main.cpp
@@ -59,11 +59,10 @@ BOOL WINAPI ControlHandle(_In_ DWORD dwCtrlType)
 void PrintUsage()
 {
     wprintf(
-        L"\n\tLogMonitor Tool Version %d.%d.%d.%d \n\n",
+        L"\n\tLogMonitor Tool Version %d.%d.%d \n\n",
         LM_MAJORNUMBER,
         LM_MINORNUMBER,
-        LM_BUILDNUMBER,
-        LM_BUILDMINORVERSION
+        LM_BUILDNUMBER
     );
     wprintf(L"\tUsage: LogMonitor.exe [/?] | [--help] | [[/CONFIG <PATH>][COMMAND [PARAMETERS]]] \n\n");
     wprintf(L"\t/?|--help   Shows help information\n");
