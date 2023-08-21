f9ac9882a6a4ac69df7b4c138d70874f94fe906a
Charity Kathure <ckathure@microsoft.com>
2023-08-22 02:45:36 +0300

    remove hacks

--- hacks/scripts/aks/rg-delete.ps1
+++
@@ -1,9 +0,0 @@
-
-# setting variables from variable file
-Write-Host "Loading variables from .\vars.txt" -ForegroundColor Yellow
-Foreach ($i in $(Get-Content vars.txt)){Set-Variable -Name $i.split("=")[0] -Value $i.split("=").split(" ")[1]}
-
-Write-Host "Deleting resouce group: $resourceGroup" -ForegroundColor Red
-az group delete --name $resourceGroup --yes --no-wait
-
-Write-Host "Completed."
